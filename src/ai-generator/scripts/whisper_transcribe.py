2722d0cf9adfb3980f347e2a4fa7b191998b37ea
Sreekanth T <sreekantht@ZHAdmins-Mac-mini2.local>
2023-11-24 20:21:55 +0530

    Font and postion corrected for the transcription

--- src/ai-generator/scripts/whisper_transcribe.py
+++ src/ai-generator/scripts/whisper_transcribe.py
@@ -1,5 +1,6 @@
 import sys
 import whisper
+import random
 
 def format_time(seconds):
     """
@@ -12,7 +13,7 @@ def format_time(seconds):
     formatted_time = f"{hours:02}:{minutes:02}:{int(seconds):02}.{centiseconds:02}"  # Note the change from comma to dot
     return formatted_time
 
-def create_ass_header():
+def create_ass_header(font_size=18, bottom_margin=50, font_path="/Users/sreekantht/Desktop/Sreekanth/GitHub/shorts-choreographer/src/data/input/fonts/impact.ttf"):
     header = [
         "[Script Info]",
         "Title: Whisper Transcription",
@@ -23,7 +24,8 @@ def create_ass_header():
         "",
         "[V4+ Styles]",
         "Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding",
-        "Style: Default, Arial, 20, &H00FFFFFF, &H00FFFFFF, &H00000000, &H00000000, 0, 0, 0, 0, 100, 100, 0, 0, 1, 2, 0, 2, 10, 10, 10, 1",
+        f"Style: Default,Impact,{font_size},&H00FFFFFF,&H00FFFFFF,&H00000000,&H80000000,-1,0,0,0,100,100,0,0,1,2,2,2,10,10,{bottom_margin},1",
+        # Added a semi-transparent black background (&H80000000) and set font to Impact
         "",
         "[Events]",
         "Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text",
@@ -31,12 +33,16 @@ def create_ass_header():
     ]
     return "\n".join(header)
 
+def get_random_color():
+    colors = ["&H00FFFFFF", "&H0000FFFF", "&H000000FF"]  # White, Yellow, Red
+    return random.choice(colors)
+
 def transcribe(audio_path, output_file_path):
     model = whisper.load_model("base")
     print("Whisper model loaded successfully.")
     result = model.transcribe(audio_path, verbose=False)
 
-    ass_header = create_ass_header()
+    ass_header = create_ass_header(font_size=18, bottom_margin=50)  # Adjust font size and bottom margin here
     with open(output_file_path, "w") as file:
         file.write(ass_header)
 
@@ -50,7 +56,8 @@ def transcribe(audio_path, output_file_path):
                 start_time = format_time(current_time)
                 current_time += time_per_word
                 end_time = format_time(current_time)
-                file.write(f"Dialogue: 0,{start_time},{end_time},Default,,0000,0000,0000,,{word}\n")
+                color = get_random_color()  # Get a random color for each word
+                file.write(f"Dialogue: 0,{start_time},{end_time},Default,,0000,0000,0000,,{{\\c{color}}}{word}{{\\3c&H000000&}}{{\\alpha&H80&}}\n")
 
 if __name__ == "__main__":
     audio_path = sys.argv[1]
