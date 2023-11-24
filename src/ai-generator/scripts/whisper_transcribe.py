539e97f352d865198d30a6a2a8401457bf7521c6
Sreekanth T <sreekantht@ZHAdmins-Mac-mini2.local>
2023-11-24 20:52:16 +0530

    Two words randomly in each trascribed subtitles

--- src/ai-generator/scripts/whisper_transcribe.py
+++ src/ai-generator/scripts/whisper_transcribe.py
@@ -1,6 +1,6 @@
 import sys
-import whisper
 import random
+import whisper
 
 def format_time(seconds):
     """
@@ -9,11 +9,10 @@ def format_time(seconds):
     hours = int(seconds // 3600)
     minutes = int((seconds % 3600) // 60)
     seconds = seconds % 60
-    centiseconds = int((seconds - int(seconds)) * 100)  # Convert milliseconds to centiseconds
-    formatted_time = f"{hours:02}:{minutes:02}:{int(seconds):02}.{centiseconds:02}"  # Note the change from comma to dot
-    return formatted_time
+    centiseconds = int((seconds - int(seconds)) * 100)
+    return f"{hours:02}:{minutes:02}:{int(seconds):02}.{centiseconds:02}"
 
-def create_ass_header(font_size=18, bottom_margin=50, font_path="/Users/sreekantht/Desktop/Sreekanth/GitHub/shorts-choreographer/src/data/input/fonts/impact.ttf"):
+def create_ass_header(font_size=18, bottom_margin=50, font_path="Impact"):
     header = [
         "[Script Info]",
         "Title: Whisper Transcription",
@@ -24,8 +23,7 @@ def create_ass_header(font_size=18, bottom_margin=50, font_path="/Users/sreekant
         "",
         "[V4+ Styles]",
         "Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding",
-        f"Style: Default,Impact,{font_size},&H00FFFFFF,&H00FFFFFF,&H00000000,&H80000000,-1,0,0,0,100,100,0,0,1,2,2,2,10,10,{bottom_margin},1",
-        # Added a semi-transparent black background (&H80000000) and set font to Impact
+        f"Style: Default, {font_path}, {font_size}, &H00FFFFFF, &H00FFFFFF, &H00000000, &H80000000, 0, 0, 0, 0, 100, 100, 0, 0, 1, 2, 2, 2, 10, 10, {bottom_margin}, 1",
         "",
         "[Events]",
         "Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text",
@@ -33,8 +31,8 @@ def create_ass_header(font_size=18, bottom_margin=50, font_path="/Users/sreekant
     ]
     return "\n".join(header)
 
-def get_random_color():
-    colors = ["&H00FFFFFF", "&H0000FFFF", "&H000000FF"]  # White, Yellow, Red
+def random_color():
+    colors = ["&H00FFFFFF", "&H0000FFFF", "&H000000FF"]  # White, Yellow, Red in BGR Hex
     return random.choice(colors)
 
 def transcribe(audio_path, output_file_path):
@@ -42,7 +40,7 @@ def transcribe(audio_path, output_file_path):
     print("Whisper model loaded successfully.")
     result = model.transcribe(audio_path, verbose=False)
 
-    ass_header = create_ass_header(font_size=18, bottom_margin=50)  # Adjust font size and bottom margin here
+    ass_header = create_ass_header()
     with open(output_file_path, "w") as file:
         file.write(ass_header)
 
@@ -52,14 +50,20 @@ def transcribe(audio_path, output_file_path):
             time_per_word = segment_duration / len(words)
 
             current_time = segment['start']
-            for word in words:
+            while words:
+                num_words = random.choice([1, 2])  # Randomly choose 1 or 2 words
+                selected_words = words[:num_words]
+                words = words[num_words:]
+
                 start_time = format_time(current_time)
-                current_time += time_per_word
+                current_time += time_per_word * len(selected_words)
                 end_time = format_time(current_time)
-                color = get_random_color()  # Get a random color for each word
-                file.write(f"Dialogue: 0,{start_time},{end_time},Default,,0000,0000,0000,,{{\\c{color}}}{word}{{\\3c&H000000&}}{{\\alpha&H80&}}\n")
+                subtitle_text = " ".join(selected_words)
+                color = random_color()
+                subtitle_line = f"Dialogue: 0,{start_time},{end_time},Default,,0000,0000,0000,,{{\\1c{color}}}{subtitle_text}\n"
+                file.write(subtitle_line)
 
 if __name__ == "__main__":
     audio_path = sys.argv[1]
-    output_ass_path = sys.argv[2]  # Second argument for the output ASS file path
+    output_ass_path = sys.argv[2]
     transcribe(audio_path, output_ass_path)
